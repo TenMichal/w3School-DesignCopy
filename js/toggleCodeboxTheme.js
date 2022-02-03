8f492bb9570cd0cfd5ad82128b1222dbc4a2d097
Michal <78083264+TenMichal@users.noreply.github.com>
2022-02-03 20:55:49 +0100

    fixes & script for dark theme

---
+++ js/toggleCodeboxTheme.js
@@ -0,0 +1,6 @@
+document.querySelector('.bxs-adjust').addEventListener('click', () => {
+    document.querySelectorAll('.example_code').forEach(box => {
+            box.classList.toggle('active')
+            box.getElementsByClassName.background = '#282C34'
+    })
+})
